a58abc589013d7be476dae0fde983cb4e040295e
Rose Weixel <rose.weixel@gmail.com>
2014-08-16 16:51:41 -0400

    Update profile and add scoped status updates

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Treebook::Application.routes.draw do
     get 'register', to: 'devise/registrations#new', as: :register
     get 'login', to: 'devise/sessions#new', as: :login
     get 'logout', to: 'devise/sessions#destroy', as: :logout
+    get 'feed', to: 'devise/statuses#index', as: :feed
   end
 
   resources :statuses
-  get 'feed', to: 'devise/statuses#index', as: :feed
+  
   root to: 'statuses#index'
+
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
