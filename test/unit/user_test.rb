8bd2517dbb635d2f6a67684bc350aa7bef6a2d58
Rose Weixel <rose.weixel@gmail.com>
2014-08-20 11:26:08 -0400

    Add users and user friendships with associated tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	should have_many(:user_friendships)
+	should have_many(:friends)
+
 	test "a user should enter a first name" do
 		user = User.new
 		assert !user.save
@@ -44,6 +47,18 @@ class UserTest < ActiveSupport::TestCase
 
 		user.profile_name = 'roseweixel_2'
 		assert user.valid?
+	end
+
+	test "that no error is raised when trying to access a friend list" do
+		assert_nothing_raised do
+			users(:rose).friends
+		end
+	end
+
+	test "that creating friendships on a user works" do
+		users(:rose).friends << users(:boatcat)
+		users(:rose).friends.reload
+		assert users(:rose).friends.include?(users(:boatcat))
 	end
 end
 
